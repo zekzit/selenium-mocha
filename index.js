9420c67da907966b10bc88830603a65279cd7600
Seksit Disaro <zekzit@gmail.com>
2018-04-26 18:12:06 +0700

    input example

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-const {Builder, By, Key, until} = require('selenium-webdriver');
-const should = require('should');
-const assert = require('assert');
-let driver = new Builder()
-    .forBrowser('chrome')
-    .build();
+const { Builder, By, Key, until } = require("selenium-webdriver");
+const should = require("should");
+const assert = require("assert");
+let driver = new Builder().forBrowser("chrome").build();
 
-driver.get('http://www.aplus-smarthome.com/');
-driver.getTitle().then((title) => {
-    assert(title === 'aplus-smarthome');
-});
+async function main() {
+  try {
+    await driver.get("http://localhost:8080/imed/index.jsp");
+    // await driver.switchTo().frame(By.name("mainFrame"));
+    await driver.switchTo().frame("mainFrame");
+    await driver.findElement(By.name("loginName")).sendKeys("imhi");
+    await driver.findElement(By.name("password")).sendKeys("demo");
+    setTimeout(async () => {
+      await driver.findElement(By.name("password")).sendKeys(Key.RETURN);
+    }, 1000);
+  } catch (e) {
+    console.log(e);
+  } finally {
+    setTimeout(() => {
+      driver.quit();
+    }, 10000);
+  }
+}
 
-// driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-// driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-driver.quit();
+main()
+  .then(() => console.log("complete"))
+  .catch(err => console.log(err));
